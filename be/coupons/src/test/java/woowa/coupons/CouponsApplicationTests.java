962da8915eae708198d2aef4ad91716e8b1179dc
JJONSOO <pjs970616@naver.com>
2023-10-11 14:28:12 +0900

    chore: 프로젝트 초기 세팅

---
+++ be/coupons/src/test/java/woowa/coupons/CouponsApplicationTests.java
@@ -0,0 +1,13 @@
+package woowa.coupons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
