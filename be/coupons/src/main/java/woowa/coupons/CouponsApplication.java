badadf5ca1ce36a3ec908d41e18457c18d1f0d0c
JJONSOO <pjs970616@naver.com>
2023-10-11 14:39:45 +0900

    style: Tab을 Space로 표현하도록 수정

--- be/coupons/src/main/java/woowa/coupons/CouponsApplication.java
+++ be/coupons/src/main/java/woowa/coupons/CouponsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CouponsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CouponsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CouponsApplication.class, args);
+    }
 
 }
--- be/coupons/src/test/java/woowa/coupons/CouponsApplicationTests.java
+++ be/coupons/src/test/java/woowa/coupons/CouponsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CouponsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
