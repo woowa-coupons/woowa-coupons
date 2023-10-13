e9b945af1b97f173574d264e99c50bec5ec85ecb
JJONSOO <pjs970616@naver.com>
2023-10-13 22:17:50 +0900

    test: signUp, signIn 인수 테스트

---
+++ be/promotion/src/test/java/woowa/promotion/acceptance/MemberAcceptanceTest.java
@@ -0,0 +1,56 @@
+package woowa.promotion.acceptance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import woowa.promotion.util.AcceptanceTest;
+
+public class MemberAcceptanceTest extends AcceptanceTest {
+
+
+    @Test
+    void signUp() {
+        // given
+        var request = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(Map.of("email", "june@codesquad.kr", "nickname", "june", "password", "password"));
+
+        // when
+        var response = request
+                .when()
+                .post("/app/auth/sign-up")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+    }
+
+    @Test
+    void signIn() throws Exception {
+        // given
+        makeMember();
+
+        var request = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(Map.of("email", "june@codesquad.kr", "password", "password"));
+
+        // when
+        var response = request
+                .when()
+                .post("/app/auth/sign-in")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+}
