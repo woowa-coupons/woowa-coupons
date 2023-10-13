c6c259e50c3651373f2787f564a2b113a1f5564d
JJONSOO <pjs970616@naver.com>
2023-10-13 22:30:08 +0900

    feat: signUpServiceRequest 생성

---
+++ be/promotion/src/main/java/woowa/promotion/app/member/application/dto/request/SignUpServiceRequest.java
@@ -0,0 +1,14 @@
+package woowa.promotion.app.member.application.dto.request;
+
+import woowa.promotion.app.member.domain.Member;
+
+public record SignUpServiceRequest(
+        String nickname,
+        String email,
+        String password
+) {
+    public Member toEntity() {
+        return new Member(nickname, email, password);
+
+    }
+}
