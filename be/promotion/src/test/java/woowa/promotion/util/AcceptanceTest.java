ba4ff6b12fa74656fbde6ad666a1a93a7cac6f6b
JJONSOO <pjs970616@naver.com>
2023-10-13 22:12:52 +0900

    test: @Transactional 추가 , supportRepository, makeMember 추가

--- be/promotion/src/test/java/woowa/promotion/util/AcceptanceTest.java
+++ be/promotion/src/test/java/woowa/promotion/util/AcceptanceTest.java
@@ -1,10 +1,31 @@
 package woowa.promotion.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import woowa.promotion.acceptance.SupportRepository;
+import woowa.promotion.app.member.domain.Member;
+import woowa.promotion.app.member.security.hash.PasswordEncoder;
+import woowa.promotion.fixture.FixtureFactory;
 
+@Transactional
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 public abstract class AcceptanceTest {
+
+    @Autowired
+    protected SupportRepository supportRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    protected Member makeMember() throws Exception {
+        String password = passwordEncoder.encrypt("password");
+        Member save = supportRepository.save(FixtureFactory.createMember(password));
+        return save;
+    }
+
 }
+
