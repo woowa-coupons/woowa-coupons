36da9f72a143881f0eac42c329c664d4b1b5238b
JJONSOO <75569293+JJONSOO@users.noreply.github.com>
2023-10-13 11:54:52 +0900

    [BE][feat] Jwt 구현, Submodule 추가 (#12)
    * chore: application-jwt 파일 추가
    
    * chore: application-jwt include
    
    * chore: jwt dependency 추가
    
    * chore: submodule dependency 추가
    
    * feat: JwtProperties 추가
    
    * chore: submodule secret 폴더 추가
    
    * feat: AccessToken 발급하는 JwtProvider
    
    * feat: @ConfigurationProperties 인식할 수 있는 @ConfigurationPropertiesScan 추가
    
    * chore: .gitmodules 추가

---
+++ be/promotion/src/main/java/woowa/promotion/global/domain/jwt/JwtProvider.java
@@ -0,0 +1,36 @@
+package woowa.promotion.global.domain.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+import org.springframework.stereotype.Component;
+import woowa.promotion.global.properties.JwtProperties;
+
+@Component
+public class JwtProvider {
+
+    public static final long ACCESS_TOKEN_EXPIRATION_TIME = 1000L * 60 * 60 * 24;
+
+    private final JwtProperties jwtProperties;
+    private final Key key;
+
+    public JwtProvider(JwtProperties jwtProperties) {
+        this.jwtProperties = jwtProperties;
+        this.key = Keys.hmacShaKeyFor(jwtProperties.secretKey().getBytes());
+    }
+
+    public String createAccessToken(Map<String, Object> claims) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(getExpireDate(ACCESS_TOKEN_EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public Date getExpireDate(long expirationTime) {
+        return new Date(System.currentTimeMillis() + expirationTime);
+    }
+
+}
