2e8421f9c202c7a9db2261a6da642964f28d778e
JJONSOO <pjs970616@naver.com>
2023-10-13 22:28:52 +0900

    feat: SHA 256 Bean 생성

---
+++ be/promotion/src/main/java/woowa/promotion/global/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package woowa.promotion.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import woowa.promotion.app.member.security.hash.PasswordEncoder;
+import woowa.promotion.app.member.security.hash.SHA256;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new SHA256();
+    }
+}
