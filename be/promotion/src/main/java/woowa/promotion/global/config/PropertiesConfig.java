3ee468a1ce5aed821d6b89de5ae90da862b3d127
JJONSOO <pjs970616@naver.com>
2023-10-13 11:21:38 +0900

    feat: @ConfigurationProperties 인식할 수 있는 @ConfigurationPropertiesScan 추가

---
+++ be/promotion/src/main/java/woowa/promotion/global/config/PropertiesConfig.java
@@ -0,0 +1,9 @@
+package woowa.promotion.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("woowa.promotion.global.properties")
+public class PropertiesConfig {
+}
