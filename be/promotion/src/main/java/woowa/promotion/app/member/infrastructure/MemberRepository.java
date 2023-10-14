089c556c7384d1981eb7b1c3fa70d359418b344c
JJONSOO <pjs970616@naver.com>
2023-10-14 12:46:56 +0900

    style: 공백 추가

--- be/promotion/src/main/java/woowa/promotion/app/member/infrastructure/MemberRepository.java
+++ be/promotion/src/main/java/woowa/promotion/app/member/infrastructure/MemberRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import woowa.promotion.app.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     Optional<Member> findByNickname(String nickname);
 
     Optional<Member> findByEmail(String email);
