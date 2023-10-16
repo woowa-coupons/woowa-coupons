83bf3e6854665f9e9b76b2ca8cc0246ffb83fe7b
JJONSOO <pjs970616@naver.com>
2023-10-16 15:05:28 +0900

    feat: 중복 이메일 처리 로직 추가

--- be/promotion/src/main/java/woowa/promotion/app/member/application/MemberService.java
+++ be/promotion/src/main/java/woowa/promotion/app/member/application/MemberService.java
@@ -1,20 +1,18 @@
 package woowa.promotion.app.member.application;
 
-import static woowa.promotion.global.exception.ErrorCode.INVALID_PASSWORD;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 import woowa.promotion.app.member.application.dto.request.SignInServiceRequest;
 import woowa.promotion.app.member.application.dto.request.SignUpServiceRequest;
 import woowa.promotion.app.member.application.dto.response.SignInServiceResponse;
 import woowa.promotion.app.member.domain.Member;
 import woowa.promotion.app.member.infrastructure.MemberRepository;
-import woowa.promotion.app.member.security.hash.PasswordEncoder;
 import woowa.promotion.global.domain.jwt.JwtProvider;
-import woowa.promotion.global.exception.ErrorCode;
+import woowa.promotion.global.exception.ApiException;
+import woowa.promotion.global.exception.domain.MemberException;
+import woowa.promotion.global.security.hash.PasswordEncoder;
 
 @RequiredArgsConstructor
 @Transactional
@@ -25,22 +23,29 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public void signUp(SignUpServiceRequest request) throws Exception {
+    public void signUp(SignUpServiceRequest request) {
         String nickname = request.nickname();
         String email = request.email();
         String encryptedPassword = passwordEncoder.encrypt(request.password());
 
+        if (isDuplicatedEmail(email)) {
+            throw new ApiException(MemberException.DUPLICATED_EMAIL);
+        }
         Member member = new Member(nickname, email, encryptedPassword);
 
         memberRepository.save(member);
     }
 
+    private boolean isDuplicatedEmail(String email) {
+        return memberRepository.existsByEmail(email);
+    }
+
     @Transactional(readOnly = true)
-    public SignInServiceResponse signIn(SignInServiceRequest request) throws Exception {
+    public SignInServiceResponse signIn(SignInServiceRequest request) {
         Member findUser = findByEmail(request.email());
 
         if (!findUser.isSamePassword(passwordEncoder.encrypt(request.password()))) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, INVALID_PASSWORD.getContent());
+            throw new ApiException(MemberException.INVALID_PASSWORD);
         }
 
         String accessToken = jwtProvider.createAccessTokenByMemberId(findUser.getId());
@@ -50,7 +55,7 @@ public class MemberService {
     private Member findByEmail(String email) {
         return memberRepository.findByEmail(email)
                 .orElseThrow(() ->
-                        new ResponseStatusException(HttpStatus.UNAUTHORIZED, ErrorCode.INVALID_EMAIL.getContent()));
+                        new ApiException(MemberException.INVALID_EMAIL));
     }
 
 }
