e6dc3318dda0b45b57b73e736bf70fe607b81e1e
JJONSOO <pjs970616@naver.com>
2023-10-16 15:06:22 +0900

    test: ParameterizedTest 추가

--- be/promotion/src/test/java/woowa/promotion/acceptance/MemberAcceptanceTest.java
+++ be/promotion/src/test/java/woowa/promotion/acceptance/MemberAcceptanceTest.java
@@ -1,23 +1,29 @@
 package woowa.promotion.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static woowa.promotion.fixture.UserFixture.유저_June;
 
 import io.restassured.RestAssured;
-import java.util.Map;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import woowa.promotion.app.member.presentation.dto.request.SignInRequest;
+import woowa.promotion.app.member.presentation.dto.request.SignUpRequest;
 import woowa.promotion.util.AcceptanceTest;
 
 public class MemberAcceptanceTest extends AcceptanceTest {
 
-    @Test
-    void signUp() {
+    @ParameterizedTest
+    @MethodSource("providerSignupUser")
+    void signUp(String email, String nickname, String password) {
         // given
         var request = RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(Map.of("email", "june@codesquad.kr", "nickname", "june", "password", "password"));
+                .body(new SignUpRequest(email, nickname, password));
 
         // when
         var response = request
@@ -30,15 +36,16 @@ public class MemberAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
-    @Test
-    void signIn() throws Exception {
+    @ParameterizedTest
+    @MethodSource("providerSigninUser")
+    void signIn(String email, String password) throws Exception {
         // given
-        makeMember();
+        makeMember(유저_June);
 
         var request = RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(Map.of("email", "june@codesquad.kr", "password", "password"));
+                .body(new SignInRequest(email, password));
 
         // when
         var response = request
@@ -48,7 +55,27 @@ public class MemberAcceptanceTest extends AcceptanceTest {
                 .extract();
 
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
     }
+
+    private static Stream<Arguments> providerSignupUser() {
+        return Stream.of(
+                Arguments.of(
+                        유저_June.getEmail(),
+                        유저_June.getNickname(),
+                        유저_June.getPassword()
+                )
+        );
+    }
+
+    private static Stream<Arguments> providerSigninUser() {
+        return Stream.of(
+                Arguments.of(
+                        유저_June.getEmail(),
+                        유저_June.getPassword()
+                )
+        );
+    }
+
 
 }
