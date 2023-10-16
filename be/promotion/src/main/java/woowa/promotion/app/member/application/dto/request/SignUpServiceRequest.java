a801c5e811fddce26f4b10c40c2c80b228c73618
JJONSOO <pjs970616@naver.com>
2023-10-16 14:57:40 +0900

    fix: 매개변수 순서 바꾸기

--- be/promotion/src/main/java/woowa/promotion/app/member/application/dto/request/SignUpServiceRequest.java
+++ be/promotion/src/main/java/woowa/promotion/app/member/application/dto/request/SignUpServiceRequest.java
@@ -1,14 +1,9 @@
 package woowa.promotion.app.member.application.dto.request;
 
-import woowa.promotion.app.member.domain.Member;
-
 public record SignUpServiceRequest(
-        String nickname,
         String email,
+        String nickname,
         String password
 ) {
-    public Member toEntity() {
-        return new Member(nickname, email, password);
-    }
-    
+
 }
