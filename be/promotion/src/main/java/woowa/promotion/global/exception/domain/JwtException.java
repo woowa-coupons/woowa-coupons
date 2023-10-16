e436ca2682beb9f40a829d6b26e732e3bfabf515
JJONSOO <pjs970616@naver.com>
2023-10-16 14:57:13 +0900

    feat: JwtException 생성

---
+++ be/promotion/src/main/java/woowa/promotion/global/exception/domain/JwtException.java
@@ -0,0 +1,17 @@
+package woowa.promotion.global.exception.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import woowa.promotion.global.exception.CustomException;
+
+@Getter
+@RequiredArgsConstructor
+public enum JwtException implements CustomException {
+
+    FAILED_ENCRYPTION(HttpStatus.INTERNAL_SERVER_ERROR, "잘못된 비밀번호입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+    
+}
