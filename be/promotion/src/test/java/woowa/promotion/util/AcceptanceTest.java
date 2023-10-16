0c565cef8885a0893648fdc0d23af5e7a57d306c
JJONSOO <pjs970616@naver.com>
2023-10-16 14:54:54 +0900

    fix: port randomd으로 바꾼 후 RestAssured port지정

--- be/promotion/src/test/java/woowa/promotion/util/AcceptanceTest.java
+++ be/promotion/src/test/java/woowa/promotion/util/AcceptanceTest.java
@@ -1,19 +1,31 @@
 package woowa.promotion.util;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 import woowa.promotion.acceptance.SupportRepository;
 import woowa.promotion.app.member.domain.Member;
-import woowa.promotion.app.member.security.hash.PasswordEncoder;
 import woowa.promotion.fixture.FixtureFactory;
+import woowa.promotion.fixture.UserFixture;
+import woowa.promotion.global.security.hash.PasswordEncoder;
 
 @Transactional
-@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public abstract class AcceptanceTest {
+    
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 
     @Autowired
     protected SupportRepository supportRepository;
@@ -21,9 +33,9 @@ public abstract class AcceptanceTest {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    protected Member makeMember() throws Exception {
-        String password = passwordEncoder.encrypt("password");
-        return supportRepository.save(FixtureFactory.createMember(password));
+    protected Member makeMember(UserFixture userFixture) throws Exception {
+        String password = passwordEncoder.encrypt(userFixture.getPassword());
+        return supportRepository.save(FixtureFactory.createMember(userFixture, password));
     }
 }
 
