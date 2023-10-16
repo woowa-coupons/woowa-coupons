d02f977c7f6d34a47899f69d97130e51b896ae3f
JJONSOO <pjs970616@naver.com>
2023-10-16 15:01:23 +0900

    style: eol 추가

--- be/promotion/src/main/java/woowa/promotion/global/config/SecurityConfig.java
+++ be/promotion/src/main/java/woowa/promotion/global/config/SecurityConfig.java
@@ -2,8 +2,8 @@ package woowa.promotion.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import woowa.promotion.app.member.security.hash.PasswordEncoder;
-import woowa.promotion.app.member.security.hash.SHA256;
+import woowa.promotion.global.security.hash.PasswordEncoder;
+import woowa.promotion.global.security.hash.SHA256;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +12,5 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new SHA256();
     }
+
 }
