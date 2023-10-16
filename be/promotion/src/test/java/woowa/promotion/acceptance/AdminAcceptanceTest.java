c2326c5f909e7624275b2732184029f7a9541107
Jeongyong Park <1239824@naver.com>
2023-10-16 17:52:50 +0900

    test: 관리자 회원가입 인수테스트 작성

---
+++ be/promotion/src/test/java/woowa/promotion/acceptance/AdminAcceptanceTest.java
@@ -0,0 +1,33 @@
+package woowa.promotion.acceptance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import woowa.promotion.fixture.FixtureFactory;
+import woowa.promotion.util.AcceptanceTest;
+
+@DisplayName("[인수테스트] 관리자")
+public class AdminAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("관리자가 회원가입에 성공한다.")
+    @Test
+    void signup() {
+        // given
+        var request = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(FixtureFactory.createSignupRequest());
+
+        // when
+        var response = request
+                .when().post("/admin/auth/sign-up")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(201);
+    }
+}
--- be/promotion/src/test/java/woowa/promotion/util/AcceptanceTest.java
+++ be/promotion/src/test/java/woowa/promotion/util/AcceptanceTest.java
@@ -3,36 +3,27 @@ package woowa.promotion.util;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import woowa.promotion.acceptance.SupportRepository;
 import woowa.promotion.app.member.domain.Member;
 import woowa.promotion.fixture.FixtureFactory;
 import woowa.promotion.fixture.UserFixture;
 import woowa.promotion.global.security.hash.PasswordEncoder;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-@Sql(value = {"classpath:schema.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Acceptance
 public abstract class AcceptanceTest {
 
+    @Autowired
+    protected SupportRepository supportRepository;
     @LocalServerPort
     private int port;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
     }
-
-    @Autowired
-    protected SupportRepository supportRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     protected Member makeMember(UserFixture userFixture) {
         String password = passwordEncoder.encrypt(userFixture.getPassword());
