d94b8c56ced5474add9e5359a68bd887829f86eb
jinny-l <jinny.l.dev@gmail.com>
2023-10-16 16:51:14 +0900

    refactor: origin URL을 배열로 받는 형태로 수정

--- be/promotion/src/main/java/woowa/promotion/global/config/web/CorsConfig.java
+++ be/promotion/src/main/java/woowa/promotion/global/config/web/CorsConfig.java
@@ -8,16 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${front.local.url}")
-    private String frontLocalUrl;
-
-    @Value("${front.server.url}")
-    private String frontServerUrl;
+    @Value("${front.url}")
+    private String[] frontUrls;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontLocalUrl, frontServerUrl)
+                .allowedOrigins(frontUrls)
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE");
     }
 }
