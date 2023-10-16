4e0f6e6ea504f10d7177c14a3c6edd52595f26ff
JJONSOO <pjs970616@naver.com>
2023-10-16 14:56:16 +0900

    test: UserFixture 생성

--- be/promotion/src/test/java/woowa/promotion/fixture/FixtureFactory.java
+++ be/promotion/src/test/java/woowa/promotion/fixture/FixtureFactory.java
@@ -3,7 +3,7 @@ package woowa.promotion.fixture;
 import woowa.promotion.app.member.domain.Member;
 
 public class FixtureFactory {
-    public static Member createMember(String password) {
-        return new Member("june", "june@codesquad.kr", password);
+    public static Member createMember(UserFixture userFixture, String password) {
+        return new Member(userFixture.getNickname(), userFixture.getEmail(), password);
     }
 }
---
+++ be/promotion/src/test/java/woowa/promotion/fixture/UserFixture.java
@@ -0,0 +1,29 @@
+package woowa.promotion.fixture;
+
+
+public enum UserFixture {
+
+    유저_June("june", "june@codesquad.kr", "password");
+
+    private final String nickname;
+    private final String email;
+    private final String password;
+
+    UserFixture(String nickname, String email, String password) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
