364072cb3d95a1dedb5a50bca910c437791c9232
Jeongyong Park <1239824@naver.com>
2023-10-17 18:23:04 +0900

    test: 관리자 로그인 REST Docs 테스트코드 작성

--- be/promotion/src/test/java/woowa/promotion/admin/documentation/AuthDocumentationTest.java
+++ be/promotion/src/test/java/woowa/promotion/admin/documentation/AuthDocumentationTest.java
@@ -5,16 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import woowa.promotion.admin.admin.application.AuthService;
-import woowa.promotion.admin.admin.application.dto.request.SignupServiceRequest;
+import woowa.promotion.admin.admin.application.dto.request.SignInServiceRequest;
 import woowa.promotion.fixture.FixtureFactory;
 import woowa.promotion.util.DocumentationTest;
 
-import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("[RESTDocs] 관리자 인증 API")
@@ -27,7 +27,7 @@ public class AuthDocumentationTest extends DocumentationTest {
     @Test
     void signup() throws Exception {
         // given
-        SignupServiceRequest signupData = FixtureFactory.createSignupServiceRequest();
+        var signupData = FixtureFactory.createSignupServiceRequest();
         willDoNothing().given(authService).signup(signupData);
 
         // when
@@ -47,6 +47,37 @@ public class AuthDocumentationTest extends DocumentationTest {
                                 fieldWithPath("email").description("이메일"),
                                 fieldWithPath("nickname").description("닉네임"),
                                 fieldWithPath("password").description("비밀번호")
+                        )
+                ));
+    }
+
+    @DisplayName("관리자 로그인")
+    @Test
+    void signIn() throws Exception {
+        // given
+        var signInData = FixtureFactory.createSignInRequest();
+        given(authService.signIn(any(SignInServiceRequest.class))).willReturn(FixtureFactory.createSignInServiceResponse());
+
+        // when
+        var response = mockMvc.perform(post("/admin/auth/sign-in")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(signInData)));
+
+        // then
+        var resultActions = response.andExpect(status().isOk())
+                .andExpect(jsonPath("accessToken").isNotEmpty());
+
+        // docs
+        resultActions
+                .andDo(document("admin/auth/signin",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("email").description("로그인 이메일"),
+                                fieldWithPath("password").description("로그인 비밀번호")
+                        ),
+                        responseFields(
+                                fieldWithPath("accessToken").description("액세스 토큰")
                         )
                 ));
     }
