96fb22829f4ba8d63a27e297b8f6c518557e71a6
jinny-l <jinny.l.dev@gmail.com>
2023-10-17 15:02:18 +0900

    feat: 회원/어드민 AuthArgumentResolver 기능 구현

---
+++ be/promotion/src/main/java/woowa/promotion/global/config/web/ArgumentResolverConfig.java
@@ -0,0 +1,20 @@
+package woowa.promotion.global.config.web;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import woowa.promotion.global.resolver.AuthArgumentResolver;
+
+@Configuration
+@RequiredArgsConstructor
+public class ArgumentResolverConfig implements WebMvcConfigurer {
+
+    private final AuthArgumentResolver authArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authArgumentResolver);
+    }
+}
