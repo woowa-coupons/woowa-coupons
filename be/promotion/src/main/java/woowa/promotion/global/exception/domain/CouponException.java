37c6f092f33e6eee367e97326efccf274324febe
jinny-l <jinny.l.dev@gmail.com>
2023-10-18 14:28:41 +0900

    feat: CouponType 정적 팩토리 메서드 구현

---
+++ be/promotion/src/main/java/woowa/promotion/global/exception/domain/CouponException.java
@@ -0,0 +1,17 @@
+package woowa.promotion.global.exception.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import woowa.promotion.global.exception.CustomException;
+
+@Getter
+@RequiredArgsConstructor
+public enum CouponException implements CustomException {
+
+    INVALID_COUPON_TYPE(HttpStatus.BAD_REQUEST, "잘못된 쿠폰 타입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+}
