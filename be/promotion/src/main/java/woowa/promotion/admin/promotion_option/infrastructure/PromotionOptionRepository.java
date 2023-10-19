56bf4e4bef284d9d0e91da43eab2b5b04fc4c15f
JJONSOO <pjs970616@naver.com>
2023-10-19 16:40:28 +0900

    feat: PromotionRepository 생성

---
+++ be/promotion/src/main/java/woowa/promotion/admin/promotion_option/infrastructure/PromotionOptionRepository.java
@@ -0,0 +1,9 @@
+package woowa.promotion.admin.promotion_option.infrastructure;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import woowa.promotion.admin.promotion_option.domain.PromotionOption;
+
+public interface PromotionOptionRepository extends JpaRepository<PromotionOption, Long> {
+    List<PromotionOption> findByPromotionId(Long id);
+}
