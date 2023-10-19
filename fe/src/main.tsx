705a6efdf9994c4748deba28197a60880157ef5f
qkdflrgs <qkdflrgs@gmail.com>
2023-10-19 17:28:39 +0900

    style: 기본 스타일 적용

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { ThemeProvider } from '@emotion/react';
+import { GlobalStyle } from '@styles/GlobalStyle.tsx';
+import { theme } from '@styles/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
