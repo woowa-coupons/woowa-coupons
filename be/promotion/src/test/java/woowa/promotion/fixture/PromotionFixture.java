f79eb459001f1729e93e1a806476323e1f1152ce
JJONSOO <pjs970616@naver.com>
2023-10-19 16:39:25 +0900

    test: Promotion Fixture 생성

---
+++ be/promotion/src/test/java/woowa/promotion/fixture/PromotionFixture.java
@@ -0,0 +1,66 @@
+package woowa.promotion.fixture;
+
+import java.time.Instant;
+import woowa.promotion.admin.promotion.domain.ProgressStatus;
+
+public enum PromotionFixture {
+
+    A_프로모션("누구나 최대 1만원", "먹고 싶은 음식을 고르세요", "www.bannerUrl.com", Instant.now(), Instant.now(),
+            "www.promotionUrl.com", true, ProgressStatus.ON_GOING.name()),
+    B_프로모션("나만 10만원", "먹기 싫은"
+            + " 음식을 고르세요", "www.bannerUrl.com", Instant.now(), Instant.now(),
+            "www.promotionUrl.com", true, ProgressStatus.ON_GOING.name());
+
+    private final String title;
+    private final String content;
+    private final String banner;
+    private final Instant startedAt;
+    private final Instant finishedAt;
+    private final String promotionPageUrl;
+    private final boolean isDisplay;
+    private final String progressStatus;
+
+    PromotionFixture(String title, String content, String banner, Instant startedAt, Instant finishedAt,
+                     String promotionPageUrl, boolean isDisplay, String progressStatus) {
+        this.title = title;
+        this.content = content;
+        this.banner = banner;
+        this.startedAt = startedAt;
+        this.finishedAt = finishedAt;
+        this.promotionPageUrl = promotionPageUrl;
+        this.isDisplay = isDisplay;
+        this.progressStatus = progressStatus;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getBanner() {
+        return banner;
+    }
+
+    public Instant getStartedAt() {
+        return startedAt;
+    }
+
+    public Instant getFinishedAt() {
+        return finishedAt;
+    }
+
+    public String getPromotionPageUrl() {
+        return promotionPageUrl;
+    }
+
+    public boolean isDisplay() {
+        return isDisplay;
+    }
+
+    public String getProgressStatus() {
+        return progressStatus;
+    }
+}
