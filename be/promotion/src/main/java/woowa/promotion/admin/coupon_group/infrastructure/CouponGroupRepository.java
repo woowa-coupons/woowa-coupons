4825b1a90080c09e637166631346d5800b121f6d
Jeongyong Park <1239824@naver.com>
2023-10-20 20:52:36 +0900

    feat: 쿠폰그룹 간단 목록 조회 기능구현

--- be/promotion/src/main/java/woowa/promotion/admin/coupon_group/infrastructure/CouponGroupRepository.java
+++ be/promotion/src/main/java/woowa/promotion/admin/coupon_group/infrastructure/CouponGroupRepository.java
@@ -1,10 +1,12 @@
 package woowa.promotion.admin.coupon_group.infrastructure;
 
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import woowa.promotion.admin.coupon_group.domain.CouponGroup;
+import woowa.promotion.admin.coupon_group.presentation.dto.response.CouponGroupSimpleResponse;
 
 public interface CouponGroupRepository extends JpaRepository<CouponGroup, Long> {
 
@@ -12,5 +14,10 @@ public interface CouponGroupRepository extends JpaRepository<CouponGroup, Long> 
             "FROM CouponGroup couponGroup " +
             "LEFT JOIN Coupon coupon ON couponGroup.id = coupon.couponGroup.id")
     Page<CouponGroup> findAll(Pageable pageable);
+
+    @Query("SELECT new woowa.promotion.admin.coupon_group.presentation.dto.response.CouponGroupSimpleResponse(" +
+            "couponGroup.id,couponGroup.title" +
+            ") FROM CouponGroup couponGroup")
+    List<CouponGroupSimpleResponse> findAllCouponGroupSimpleResponse();
 
 }
