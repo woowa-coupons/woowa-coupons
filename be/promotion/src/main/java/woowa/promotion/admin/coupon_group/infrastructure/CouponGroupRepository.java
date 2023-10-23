83ecfefabede51044cafc21c248bc078205b3a64
Jeongyong Park <1239824@naver.com>
2023-10-23 11:16:42 +0900

    style: `,`뒤 공백 추가

--- be/promotion/src/main/java/woowa/promotion/admin/coupon_group/infrastructure/CouponGroupRepository.java
+++ be/promotion/src/main/java/woowa/promotion/admin/coupon_group/infrastructure/CouponGroupRepository.java
@@ -1,6 +1,5 @@
 package woowa.promotion.admin.coupon_group.infrastructure;
 
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,12 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 import woowa.promotion.admin.coupon_group.domain.CouponGroup;
 import woowa.promotion.admin.coupon_group.presentation.dto.response.CouponGroupSimpleResponse;
 
+import java.util.List;
+
 public interface CouponGroupRepository extends JpaRepository<CouponGroup, Long> {
 
     Page<CouponGroup> findAll(Pageable pageable);
 
     @Query("SELECT new woowa.promotion.admin.coupon_group.presentation.dto.response.CouponGroupSimpleResponse(" +
-            "couponGroup.id,couponGroup.title" +
+            "couponGroup.id, couponGroup.title" +
             ") FROM CouponGroup couponGroup")
     List<CouponGroupSimpleResponse> findAllCouponGroupSimpleResponse();
 
