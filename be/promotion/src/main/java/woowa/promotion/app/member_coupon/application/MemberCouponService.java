7aa0f6a56748fba00035f88d42c3f984c0395185
jinny-l <jinny.l.dev@gmail.com>
2023-10-23 22:25:34 +0900

    feat: 회원이 프로모션에 조건에 적합한지 확인하는 로직 구현

--- be/promotion/src/main/java/woowa/promotion/app/member_coupon/application/MemberCouponService.java
+++ be/promotion/src/main/java/woowa/promotion/app/member_coupon/application/MemberCouponService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import woowa.promotion.admin.promotion_option.domain.MemberType;
+import woowa.promotion.admin.promotion_option.domain.PromotionOption;
 import woowa.promotion.app.order.infrastructure.OrderRepository;
 
 
@@ -14,6 +15,19 @@ import woowa.promotion.app.order.infrastructure.OrderRepository;
 public class MemberCouponService {
 
     private final OrderRepository orderRepository;
+
+    private boolean isEligibleForPromotion(Long memberId, PromotionOption promotionOption) {
+        if (promotionOption.getLastOrderAt() != null) {
+            // TODO 회원 마지막 주문일 체크하는 로직 구현(현 기획상 마지막 주문일 이전인지 이후인지 체크하는 기능이 없음)
+            return false;
+        }
+
+        MemberType memberType = findMemberType(memberId);
+        if (promotionOption.getMemberType().equals(memberType)) {
+            return true;
+        }
+        return false;
+    }
 
     // OrderService가 없어서 임의로 로직 생성
     private MemberType findMemberType(Long memberId) {
