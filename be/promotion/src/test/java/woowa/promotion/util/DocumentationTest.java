4dc52711785045506a45e66f346e754496dc822e
Jeongyong Park <1239824@naver.com>
2023-10-23 14:38:08 +0900

    test: 회원 프로모션 목록 조회 RESTDocs 테스트코드 작성

--- be/promotion/src/test/java/woowa/promotion/util/DocumentationTest.java
+++ be/promotion/src/test/java/woowa/promotion/util/DocumentationTest.java
@@ -1,8 +1,5 @@
 package woowa.promotion.util;
 
-import static org.mockito.BDDMockito.any;
-import static org.mockito.BDDMockito.given;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +12,23 @@ import woowa.promotion.admin.admin.domain.Admin;
 import woowa.promotion.admin.admin.presentation.AuthController;
 import woowa.promotion.admin.coupon_group.application.CouponGroupService;
 import woowa.promotion.admin.coupon_group.presentation.CouponGroupController;
+import woowa.promotion.app.promotion.application.AppPromotionService;
+import woowa.promotion.app.promotion.presentation.AppPromotionController;
 import woowa.promotion.global.interceptor.AuthInterceptor;
 import woowa.promotion.global.resolver.AuthArgumentResolver;
 
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
+
 @MockBean({
         AdminService.class,
-        CouponGroupService.class
+        CouponGroupService.class,
+        AppPromotionService.class
 })
 @WebMvcTest({
         AuthController.class,
-        CouponGroupController.class
+        CouponGroupController.class,
+        AppPromotionController.class
 })
 @AutoConfigureRestDocs
 public abstract class DocumentationTest {
