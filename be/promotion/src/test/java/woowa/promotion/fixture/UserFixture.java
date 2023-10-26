d630cc9da838a54926f81749588556646767a3d0
JJONSOO <pjs970616@naver.com>
2023-10-26 20:08:57 +0900

    test: 쿠폰 발급 시나리오 User Fixture 생성

--- be/promotion/src/test/java/woowa/promotion/fixture/UserFixture.java
+++ be/promotion/src/test/java/woowa/promotion/fixture/UserFixture.java
@@ -3,7 +3,9 @@ package woowa.promotion.fixture;
 
 public enum UserFixture {
 
-    유저_June("june", "june@codesquad.kr", "password");
+    유저_June("june", "june@codesquad.kr", "password"),
+    유저_Jinny("Jinny", "jinny@codesquad.kr", "password"),
+    유저_Bruni("Bruni", "bruni@codesquad.kr", "password");
 
     private final String nickname;
     private final String email;
