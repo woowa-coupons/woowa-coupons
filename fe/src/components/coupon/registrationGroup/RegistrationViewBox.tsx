f89b5f6ec75c0ba9f2d66ecf8f1416838ce65b12
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-10-27 04:26:57 +0900

    style: 린트 적용 (#69)

--- fe/src/components/coupon/registrationGroup/RegistrationViewBox.tsx
+++ fe/src/components/coupon/registrationGroup/RegistrationViewBox.tsx
@@ -1,31 +1,31 @@
 /** @jsxImportSource @emotion/react */
 
-import { Coupon } from "../CouponType";
-import CouponRegistrationItem from "./CouponRegistrationItem";
-import { registrationBoxStyle } from "./CouponRegistrationGroup.style";
+import { Coupon } from '../CouponType';
+import CouponRegistrationItem from './CouponRegistrationItem';
+import { registrationBoxStyle } from './CouponRegistrationGroup.style';
 
 type Props = {
-    coupons: Coupon[],
-    onCreate: (coupon: Coupon) => void,
-    onEdit: (couponId: number, newCoupon: Coupon) => void,
-    onDelete: (couponId: number) => void,
-}
+  coupons: Coupon[];
+  onCreate: (coupon: Coupon) => void;
+  onEdit: (couponId: number, newCoupon: Coupon) => void;
+  onDelete: (couponId: number) => void;
+};
 
 export default function RegistrationViewBox(props: Props) {
-
-
-
-    return (
-        <div css={registrationBoxStyle}>
-            {props.coupons.slice().reverse().map((coupon, index) => (
-                <CouponRegistrationItem
-                    key={index}
-                    index={props.coupons.length - index}
-                    coupon={coupon}
-                    onEdit={props.onEdit}
-                    onDelete={props.onDelete}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div css={registrationBoxStyle}>
+      {props.coupons
+        .slice()
+        .reverse()
+        .map((coupon, index) => (
+          <CouponRegistrationItem
+            key={index}
+            index={props.coupons.length - index}
+            coupon={coupon}
+            // onEdit={props.onEdit} TODO: 입력된 쿠폰 수정기능 추가하기
+            onDelete={props.onDelete}
+          />
+        ))}
+    </div>
+  );
 }
