30f7544ab80380a5d0b4603fd115a479cf78162c
JJONSOO <pjs970616@naver.com>
2023-10-26 20:09:04 +0900

    test: 쿠폰 발급 시나리오 PromotinoOption Fixture 생성

--- be/promotion/src/test/java/woowa/promotion/fixture/PromotionOptionFixture.java
+++ be/promotion/src/test/java/woowa/promotion/fixture/PromotionOptionFixture.java
@@ -1,22 +1,40 @@
 package woowa.promotion.fixture;
 
 import java.time.Instant;
+import java.time.Year;
+import java.time.ZoneId;
+import woowa.promotion.admin.promotion_option.domain.MemberType;
 
 public enum PromotionOptionFixture {
 
-    A_프로모션_옵션(Instant.now(), "NEW_MEMBER"),
-    B_프로모션_옵션(Instant.now(), "OLD_MEMBER");
+    A_프로모션_옵션(Instant.now(), true, MemberType.NEW_MEMBER.name()),
+    B_프로모션_옵션(Instant.now(), false, MemberType.OLD_MEMBER.name()),
+    첫번째_프로모션_옵션(getLastOrderAtPromotion(), true, MemberType.OLD_MEMBER.name()),
+    두번째_프로모션_옵션(getLastOrderAtPromotion(), false, MemberType.OLD_MEMBER.name()),
+    세번째_프로모션_옵션(null, null, MemberType.NEW_MEMBER.name()),
+    네번째_프로모션_옵션(null, null, MemberType.OLD_MEMBER.name());
+
+
+    private static Instant getLastOrderAtPromotion() {
+        return Year.of(2002).atMonth(6).atDay(18).atTime(20, 30).atZone(ZoneId.of("Asia/Seoul")).toInstant();
+    }
 
     private Instant lastOrderAt;
+    private Boolean lastOrderBefore;
     private String memberType;
 
-    PromotionOptionFixture(Instant lastOrderAt, String memberType) {
+    PromotionOptionFixture(Instant lastOrderAt, Boolean lastOrderBefore, String memberType) {
         this.lastOrderAt = lastOrderAt;
+        this.lastOrderBefore = lastOrderBefore;
         this.memberType = memberType;
     }
 
     public Instant getLastOrderAt() {
         return lastOrderAt;
+    }
+
+    public Boolean getLastOrderBefore() {
+        return lastOrderBefore;
     }
 
     public String getMemberType() {
