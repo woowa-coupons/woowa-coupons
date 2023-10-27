12bc7287d92d0f1c9a8a858b9a4b2c9214692b70
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-10-27 11:05:34 +0900

    feat: 쿠폰 그룹 타입 필드 추가 (#69)

--- fe/src/components/coupon/CouponType.tsx
+++ fe/src/components/coupon/CouponType.tsx
@@ -11,3 +11,18 @@ export type Coupon = {
   discount: number;
   quantity: number;
 };
+
+export const IsRandom = [
+    { text: '일반 발급', value: '정액' },
+    { text: '랜덤 발급', value: '정률' },
+]
+
+export const CouponGroupType = [
+    { text: '일일 발급', value: 'EVERYDAY' },
+    { text: '기간 발급', value: 'PERIOD' },
+]
+
+export const CouponType = [
+    { text: '정액 쿠폰', value: 'fixed' },
+    { text: '정률 쿠폰', value: 'rate' },
+]
