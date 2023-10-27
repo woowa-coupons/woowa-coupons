c61f9adbfab26dfb7c2a34d149e7d6c7c524b2b9
JJONSOO <pjs970616@naver.com>
2023-10-27 13:53:39 +0900

    sql: nativeQuery로 하면 boolean으로 return 이 되지 않는 걸 jqpl로 수정

--- be/promotion/src/main/java/woowa/promotion/app/member_coupon/infrastructure/MemberCouponRepository.java
+++ be/promotion/src/main/java/woowa/promotion/app/member_coupon/infrastructure/MemberCouponRepository.java
@@ -11,12 +11,11 @@ public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long
 
     boolean existsByMemberIdAndCouponIdIn(Long memberId, List<Long> couponIds);
 
-    @Query(value = "SELECT EXISTS (SELECT 1 " +
-            "FROM member_coupon " +
-            "WHERE member_id = :memberId " +
-            "AND coupon_id IN :couponIds " +
-            "AND issued_at BETWEEN :start AND :end)",
-            nativeQuery = true)
+    @Query("SELECT COUNT(memberCoupon .id) > 0  " +
+            "FROM MemberCoupon memberCoupon " +
+            "WHERE memberCoupon.member.id= :memberId " +
+            "AND memberCoupon.coupon.id IN :couponIds " +
+            "AND memberCoupon.issuedAt BETWEEN :start AND :end")
     boolean existsByMemberIdAndCouponIdInAndToday(@Param("memberId") Long memberId,
                                                   @Param("couponIds") List<Long> couponIds,
                                                   @Param("start") Instant start, @Param("end") Instant end);
