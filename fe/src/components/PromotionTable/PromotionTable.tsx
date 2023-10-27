d712488f5f4047ed4c512d6074a2bddded19fe18
qkdflrgs <qkdflrgs@gmail.com>
2023-10-27 16:18:09 +0900

    feat: Promotion Table 컴포넌트

---
+++ fe/src/components/PromotionTable/PromotionTable.tsx
@@ -0,0 +1,95 @@
+import { useTheme } from '@emotion/react';
+import {
+  bannerImg,
+  detailLink,
+  header,
+  headerTitle,
+  promotionItem,
+  sectionStyles,
+  table,
+  tableBody,
+} from './PromotionTable.styles';
+import { promotionTable } from '@constants/promotionTable';
+import { Button } from '@components/common/Button/Button';
+import { Icon } from '@components/common/Icon/Icon';
+import { buttonStyles } from '@components/common/Button/Button.styles';
+import { formatDate } from '@utils/formatData';
+
+export type PromotionList = {
+  id: number;
+  bannerUrl: string;
+  title: string;
+  startedAt: string;
+  finishedAt: string;
+  progressStatus: string;
+};
+
+type PromotionTableProps = { data: PromotionList[] };
+
+export function PromotionTable({ data }: PromotionTableProps) {
+  const theme = useTheme();
+
+  return (
+    <table css={table(theme)}>
+      <thead>
+        <tr css={header(theme)}>
+          <td css={sectionStyles.checkbox}>
+            <input type="checkbox" />
+          </td>
+          {promotionTable.map((item) => (
+            <td key={item.name} css={headerTitle(item.size)}>
+              <span>{item.name}</span>
+            </td>
+          ))}
+        </tr>
+      </thead>
+      <tbody css={tableBody(theme)}>
+        {data.map((item) => (
+          <tr key={item.id} css={promotionItem(theme, false)}>
+            <td css={sectionStyles.checkbox}>
+              <input type="checkbox" />
+            </td>
+            <td css={sectionStyles.no}>
+              <span>{item.id}</span>
+            </td>
+            <td css={sectionStyles.banner}>
+              <img
+                css={bannerImg}
+                src={item.bannerUrl}
+                alt="프로모션 배너 이미지"
+              />
+            </td>
+            <td css={sectionStyles.name}>
+              <a href={`/promotion/${item.id}`} css={detailLink(theme)}>
+                <span>{item.title}</span>
+              </a>
+            </td>
+            <td css={sectionStyles.startDate}>
+              <span>{formatDate(item.startedAt)}</span>
+            </td>
+            <td css={sectionStyles.endDate}>
+              <span>{formatDate(item.finishedAt)}</span>
+            </td>
+            <td css={sectionStyles.status}>
+              <span>{item.progressStatus}</span>
+            </td>
+            <td css={sectionStyles.writer}>
+              <span>litae</span>
+            </td>
+            <td css={sectionStyles.buttons}>
+              <Button cssProp={buttonStyles.onlyIconLarge()}>
+                <Icon name="chart" fill="none" stroke="GRAY_400" />
+              </Button>
+              <Button cssProp={buttonStyles.onlyIconLarge()}>
+                <Icon name="edit" fill="none" stroke="GRAY_400" />
+              </Button>
+              <Button cssProp={buttonStyles.onlyIconLarge()}>
+                <Icon name="trash" fill="none" stroke="GRAY_400" />
+              </Button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
