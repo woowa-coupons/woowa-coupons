f652011dbff37b905ee5fbfccec6c7d0690e78e6
jinny-l <jinny.l.dev@gmail.com>
2023-10-31 11:45:21 +0900

    fix: 쿠폰 발급 시 동시성 이슈 해결을 위해 `@Lock` 어노테이션 추가

--- be/promotion/src/main/java/woowa/promotion/admin/promotion_option_coupon_group/infrastructure/PromotionOptionCouponGroupRepository.java
+++ be/promotion/src/main/java/woowa/promotion/admin/promotion_option_coupon_group/infrastructure/PromotionOptionCouponGroupRepository.java
@@ -1,13 +1,16 @@
 package woowa.promotion.admin.promotion_option_coupon_group.infrastructure;
 
 import java.util.List;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import woowa.promotion.admin.promotion_option_coupon_group.domain.PromotionOptionCouponGroup;
 
 public interface PromotionOptionCouponGroupRepository extends JpaRepository<PromotionOptionCouponGroup, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT promotionOptionCouponGroup " +
             "FROM PromotionOptionCouponGroup promotionOptionCouponGroup " +
             "JOIN FETCH promotionOptionCouponGroup.couponGroup couponGroup " +
--- be/promotion/src/main/java/woowa/promotion/app/member_coupon/infrastructure/MemberCouponRepository.java
+++ be/promotion/src/main/java/woowa/promotion/app/member_coupon/infrastructure/MemberCouponRepository.java
@@ -2,7 +2,9 @@ package woowa.promotion.app.member_coupon.infrastructure;
 
 import java.time.Instant;
 import java.util.List;
+import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import woowa.promotion.app.member_coupon.domain.MemberCoupon;
@@ -11,6 +13,7 @@ public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long
 
     boolean existsByMemberIdAndCouponIdIn(Long memberId, List<Long> couponIds);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT COUNT(memberCoupon .id) > 0  " +
             "FROM MemberCoupon memberCoupon " +
             "WHERE memberCoupon.member.id= :memberId " +
