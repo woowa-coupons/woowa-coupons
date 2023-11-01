e506c43ba935d6b7f659b1ef4c3c6add78335fc8
Jeongyong Park <1239824@naver.com>
2023-11-01 15:16:14 +0900

    feat: `synchronized` 키워드를 이용해 동시성 문제 해결

--- be/promotion/src/test/java/woowa/promotion/util/DocumentationTest.java
+++ be/promotion/src/test/java/woowa/promotion/util/DocumentationTest.java
@@ -1,5 +1,8 @@
 package woowa.promotion.util;
 
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,20 +16,19 @@ import woowa.promotion.admin.admin.presentation.AuthController;
 import woowa.promotion.admin.coupon_group.application.CouponGroupService;
 import woowa.promotion.admin.coupon_group.presentation.CouponGroupController;
 import woowa.promotion.app.member_coupon.application.MemberCouponService;
+import woowa.promotion.app.member_coupon.application.SynchronizedOuter;
 import woowa.promotion.app.member_coupon.presentation.AppCouponController;
 import woowa.promotion.app.promotion.application.AppPromotionService;
 import woowa.promotion.app.promotion.presentation.AppPromotionController;
 import woowa.promotion.global.interceptor.AuthInterceptor;
 import woowa.promotion.global.resolver.AuthArgumentResolver;
 
-import static org.mockito.BDDMockito.any;
-import static org.mockito.BDDMockito.given;
-
 @MockBean({
         AdminService.class,
         CouponGroupService.class,
         AppPromotionService.class,
-        MemberCouponService.class
+        MemberCouponService.class,
+        SynchronizedOuter.class
 })
 @WebMvcTest({
         AuthController.class,
