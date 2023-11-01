0a78053fce947a131b3725e756aae1e3f06d619b
JeongYong Park <66981851+23Yong@users.noreply.github.com>
2023-11-01 15:09:43 +0900

    feat: `synchronized` 키워드를 이용해 동시성 문제 해결 (#103)

--- be/promotion/src/main/java/woowa/promotion/admin/coupon/infrastructure/CouponRepository.java
+++ be/promotion/src/main/java/woowa/promotion/admin/coupon/infrastructure/CouponRepository.java
@@ -1,18 +1,15 @@
 package woowa.promotion.admin.coupon.infrastructure;
 
-import java.util.List;
-import javax.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import woowa.promotion.admin.coupon.domain.Coupon;
+import java.util.List;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
     @Query("SELECT coupon.id FROM Coupon coupon WHERE coupon.couponGroup.id = :couponGroupId")
     List<Long> findIdsByCouponGroupId(@Param("couponGroupId") Long couponGroupId);
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Coupon> findByCouponGroupId(@Param("couponGroupId") Long couponGroupId);
 }
---
+++ be/promotion/src/test/java/woowa/promotion/app/member_coupon/application/MemberCouponServiceTest.java
@@ -0,0 +1,7 @@
+package woowa.promotion.app.member_coupon.application;
+
+import woowa.promotion.util.ApplicationTest;
+
+class MemberCouponServiceTest extends ApplicationTest {
+
+}
