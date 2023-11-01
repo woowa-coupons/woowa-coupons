75b286789275f25da6cf37a62a915e04512d961a
Jeongyong Park <1239824@naver.com>
2023-11-01 15:08:09 +0900

    feat: `synchronized` 키워드를 이용해 동시성 문제 해결

--- be/promotion/src/main/java/woowa/promotion/app/member_coupon/presentation/AppCouponController.java
+++ be/promotion/src/main/java/woowa/promotion/app/member_coupon/presentation/AppCouponController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import woowa.promotion.app.member.domain.Member;
-import woowa.promotion.app.member_coupon.application.MemberCouponService;
+import woowa.promotion.app.member_coupon.application.SynchronizedOuter;
 import woowa.promotion.app.member_coupon.presentation.dto.CouponIssueRequest;
 import woowa.promotion.global.resolver.Authentication;
 
@@ -17,14 +17,14 @@ import woowa.promotion.global.resolver.Authentication;
 @RestController
 public class AppCouponController {
 
-    private final MemberCouponService memberCouponService;
+    private final SynchronizedOuter synchronizedOuter;
 
     @PostMapping
     public ResponseEntity<Void> issueCoupon(
             @RequestBody CouponIssueRequest request,
             @Authentication Member member
     ) {
-        memberCouponService.issueCoupon(request, member);
+        synchronizedOuter.issueCoupon(request, member);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- be/promotion/src/test/java/woowa/promotion/application/app/MemberCouponTest.java
+++ be/promotion/src/test/java/woowa/promotion/application/app/MemberCouponTest.java
@@ -2,19 +2,10 @@ package woowa.promotion.application.app;
 
 import static woowa.promotion.util.fixture.CouponFixture.추석_쿠폰_신규;
 import static woowa.promotion.util.fixture.CouponGroupFixture.추석_쿠폰그룹_기존;
-import static woowa.promotion.util.fixture.FixtureFactory.createCoupon;
-import static woowa.promotion.util.fixture.FixtureFactory.createCouponGroup;
-import static woowa.promotion.util.fixture.FixtureFactory.createPromotion;
-import static woowa.promotion.util.fixture.FixtureFactory.createPromotionOption;
+import static woowa.promotion.util.fixture.FixtureFactory.*;
 import static woowa.promotion.util.fixture.PromotionFixture.추석_프로모션;
 import static woowa.promotion.util.fixture.PromotionOptionFixture.네번째_프로모션_옵션;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,17 +13,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import woowa.promotion.admin.promotion.domain.Promotion;
 import woowa.promotion.admin.promotion_option.domain.PromotionOption;
 import woowa.promotion.app.member.domain.Member;
-import woowa.promotion.app.member_coupon.application.MemberCouponService;
+import woowa.promotion.app.member_coupon.application.SynchronizedOuter;
 import woowa.promotion.app.member_coupon.domain.MemberCoupon;
 import woowa.promotion.app.member_coupon.presentation.dto.CouponIssueRequest;
 import woowa.promotion.util.AcceptanceTest;
 import woowa.promotion.util.fixture.CouponFixture;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @DisplayName("[비즈니스 로직 테스트][회원] 쿠폰 발급 - 동시성")
 public class MemberCouponTest extends AcceptanceTest { // ApplicationTest에 트랜잭션 어노테이션이 있어 AcceptanceTest로 실행
 
     @Autowired
-    private MemberCouponService memberCouponService;
+    private SynchronizedOuter memberCouponService;
 
     @Test
     void issueCoupon() throws InterruptedException {
