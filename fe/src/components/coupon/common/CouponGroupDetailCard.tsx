e905ec3175ab4b7d1040fbaf855aaaf6ac921f9c
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2023-11-02 15:41:27 +0900

    feat: 쿠폰 상세 보기 페이지 개발 (#99)

---
+++ fe/src/components/coupon/common/CouponGroupDetailCard.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react';
+import ViewBoxGroup from '../viewDataGroup/ViewBoxGroup';
+import ViewTextGroup from '../viewDataGroup/ViewTextGroup';
+import { cardStyle } from './CouponGroup.style';
+import { Coupon } from './CouponType';
+
+type Props = {
+  couponGroupId: number,
+}
+
+export default function CouponGroupDetailCard({couponGroupId}: Props) {
+  const [title, setTitle] = useState('');
+  const [startedAt, setStartedAt] = useState('');
+  const [finishedAt, setFinishedAt] = useState('');
+  const [type, setType] = useState('');
+  const [isRandom, setIsRandom] = useState('');
+  const [coupons, setCoupons] = useState<Coupon[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await fetchData();
+      setTitle(data.title);
+      setStartedAt(data.startedAt);
+      setFinishedAt(data.finishedAt);
+      setType(data.type);
+      setIsRandom(data.isRandom);
+      setCoupons(data.coupons);
+    })();
+  }, []);
+
+  const fetchData = async () => {
+    const baseUrl = import.meta.env.VITE_BASE_URL;
+    const options = {
+      method : "GET",
+      headers: {
+        'Authorization': 'Bearer ' + import.meta.env.VITE_ACCESS_TOKEN_FOR_TEST,
+        'Content-Type': 'application/json',
+      },
+    };
+    const url = new URL(`/admin/coupon-groups/` + couponGroupId, baseUrl);
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch');
+    }
+
+    const data = await response.json();
+
+    return data;
+  };
+
+  return (
+    <div css={cardStyle}>
+      <ViewTextGroup title={'쿠폰 그룹 No.'} content={'-'} size={'medium'} />
+
+      <ViewTextGroup title={'쿠폰 그룹 이름'} content={title} size={'medium'} />
+
+      <ViewTextGroup title={'발급 시작 일자'} content={startedAt} size={'medium'} />
+
+      <ViewTextGroup title={'발급 종료 일자'} content={finishedAt} size={'medium'} />
+
+      <ViewTextGroup title={'랜덤 발급 여부'} content={isRandom? '랜덤 발급' : 'X'} size={'medium'} />
+
+      <ViewTextGroup title={'쿠폰 그룹 타입'} content={type} size={'medium'} />
+
+      <ViewBoxGroup title={'등록된 쿠폰'} content={coupons} />
+    </div>
+  );
+}
